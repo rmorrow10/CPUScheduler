7826554d8dbb385d5cd2449b0c7545358abdcd78
rmorrow10 <59890036+rmorrow10@users.noreply.github.com>
2021-03-10 17:46:23 -0600

    Create Formula.java
    Updated Formula.java

---
+++ Formula.java
@@ -0,0 +1,73 @@
+/**
+ * @author Riley Morrow
+ * Date: March 10, 2021
+ * Description: A class file to define the variables and functions for the CPU Scheduler
+ */
+
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+
+public class Formula {
+	
+	private OperatingSystemMXBean mbean = (com.sun.management.OperatingSystemMXBean)
+	ManagementFactory.getOperatingSystemMXBean();
+	
+	private double CPUUtilization;
+	private double throughput;
+	private double turnaroundTime;
+	private double waitingTime;
+	private double responseTime;
+	
+	public Formula() {
+	}
+	
+	public void setCPUUtilization() {
+		this.CPUUtilization = CPUUtilization;
+	}
+	
+	public double getCPUUtilization(OperatingSystemMXBean mbean) {
+		CPUUtilization = mbean.getSystemCpuLoad() * 100.0;
+		return CPUUtilization;
+	}
+	
+	public void setThroughput() {
+		this.throughput = throughput;
+	}
+	
+	public double getThroughput() {
+		//long startTime = System.nanoTime();
+		//int threads;
+		long endTime = System.nanoTime();
+		//double timeDiffSeconds = (endTime - startTime) / 1000000000.0;
+		//throughput = (threads / timeDiffSeconds);
+		return throughput;
+	}
+	
+	public void setTurnaroundTime() {
+		this.turnaroundTime = turnaroundTime;
+	}
+	
+	public double getTurnaroundTime(double exitTime, double arrivalTime) {
+		turnaroundTime = exitTime - arrivalTime;
+		return turnaroundTime;
+	}
+	
+	public void setWaitingTime() {
+		this.waitingTime = waitingTime;
+	}
+	
+	public double getWaitingTime(double turnaroundTime, double burstTime) {
+		waitingTime = turnaroundTime - burstTime;
+		return waitingTime;
+	}
+	
+	public void setResponseTime() {
+		this.responseTime = responseTime;
+	}
+	
+	public double getResponseTime(double firstCPUTime, double arrivalTime) {
+		responseTime = firstCPUTime - arrivalTime;
+		return responseTime;
+	}
+
+}
